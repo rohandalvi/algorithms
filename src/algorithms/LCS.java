af7ee7516569702a98a02a55c2b9890be816383b
Rohan Dalvi <rohan.dalvi@bronto.com>
2017-03-23 19:02:45 -0400

    Added method to print the actual longest common subsequence string

--- src/algorithms/LCS.java
+++ src/algorithms/LCS.java
@@ -1,12 +1,17 @@
 package algorithms;
 
 public class LCS {
+  static String[][] pointer;
+
   public static void main(String[] args) {
     String a = "ACBEA";
     String b = "ADCA";
 
     System.out.println(lcs(a, b, a.length() - 1, b.length() - 1));
     System.out.println(lcsDP(a, b));
+
+    // Once pointer table is populated.
+    System.out.println("LCS string " + getLCSString(a));
   }
 
   public static int lcs(String a, String b, int m, int n) {
@@ -24,19 +29,45 @@ public class LCS {
     int aLength = a.length();
     int bLength = b.length();
     int[][] memo = new int[aLength][bLength];
+    pointer = new String[aLength][bLength];
 
     for (int i = 0; i < aLength; i++) {
       for (int j = 0; j < bLength; j++) {
         if (a.charAt(i) == b.charAt(j)) {
           int val = (i == 0 || j == 0) ? 0 : memo[i - 1][j - 1];
           memo[i][j] = val + 1;
+          pointer[i][j] = "S3";
         } else {
           int val1 = i == 0 ? 0 : memo[i - 1][j];
           int val2 = j == 0 ? 0 : memo[i][j - 1];
+          if (val1 > val2) {
+            pointer[i][j] = "S1";
+          } else {
+            pointer[i][j] = "S2";
+          }
           memo[i][j] = Math.max(val2, val1);
         }
       }
     }
     return memo[aLength - 1][bLength - 1];
+  }
+
+  public static String getLCSString(String a) {
+    int i = pointer.length - 1;
+    int j = pointer[i].length - 1;
+    StringBuilder sb = new StringBuilder();
+    while (i >= 0 && j >= 0) {
+      if (pointer[i][j].equals("S3")) {
+        sb.append(a.charAt(i));
+        i--;
+        j--;
+
+      } else if (pointer[i][j].equals("S2")) {
+        j--;
+      } else {
+        i--;
+      }
+    }
+    return sb.reverse().toString();
   }
 }
