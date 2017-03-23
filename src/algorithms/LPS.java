5d03e3cb43edf006c36ca77b1427f1010c2e13b7
Rohan Dalvi <rohan.dalvi@bronto.com>
2017-03-23 18:20:15 -0400

    Added lpsDP and also removed unnecessary map from naive solution

--- src/algorithms/LPS.java
+++ src/algorithms/LPS.java
@@ -1,24 +1,42 @@
 package algorithms;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LPS {
   public static void main(String[] args) {
     String a = "LPASPAL";
     int start = 0;
     int end = a.length() - 1;
-    Map<String, Integer> map = new HashMap<>();
-    System.out.println(lps("LPASPAL", start, end, map));
+    System.out.println("LPS Naive " + lpsNaive(a, start, end));
+    System.out.println("LPS DP " + lpsDP(a));
   }
 
-  public static int lps(String a, int start, int end, Map<String, Integer> map) {
+  public static int lpsNaive(String a, int start, int end) {
     if (start >= end)
       return 0;
     if (a.charAt(start) == a.charAt(end)) {
-      return 2 + lps(a, start + 1, end - 1, map);
+      return 2 + lpsNaive(a, start + 1, end - 1);
     } else {
-      return Math.max(lps(a, start + 1, end, map), lps(a, start, end - 1, map));
+      return Math.max(lpsNaive(a, start + 1, end), lpsNaive(a, start, end - 1));
     }
+  }
+
+  public static int lpsDP(String a) {
+    int n = a.length();
+    int[][] memo = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      memo[i][i] = 1;
+    }
+
+    for (int currLen = 2; currLen <= n; currLen++) {
+      for (int i = 0; i < n - currLen + 1; i++) {
+        int j = i + currLen - 1;
+        if (a.charAt(i) == a.charAt(j)) {
+          memo[i][j] = 2 + memo[i + 1][j - 1];
+        } else {
+          memo[i][j] = Math.max(memo[i + 1][j], memo[i][j - 1]);
+        }
+      }
+    }
+    return memo[0][n - 1];
   }
 }
