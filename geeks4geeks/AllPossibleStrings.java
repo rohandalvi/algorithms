0880ee134e937b3d6fd2865ad51aba5c94691e38
Rohan Dalvi <rohan_dalvi@me.com>
2017-04-28 16:05:16 -0400

    Added new question to generate all possible strings with 0 or 1 spaces in between

---
+++ geeks4geeks/AllPossibleStrings.java
@@ -0,0 +1,27 @@
+package geeks4geeks;
+
+public class AllPossibleStrings {
+
+  /**
+   * Given a string str your task is to complete the function printSpaceString which takes only one argument the string
+   * str and prints all possible strings that can be made by placing spaces (zero or one) in between them.
+   * 
+   * For eg . for the string abc all valid strings will be abc$ ab c$ a bc$ a b c$
+   * 
+   * @param str
+   */
+  public void printSpace(String str) {
+    int n = str.length();
+    dfs(1, n, "" + str.charAt(0), str);
+  }
+
+  private void dfs(int index, int length, String temp, String str) {
+    if (index == length) {
+      System.out.println(temp + "$");
+      return;
+    }
+
+    dfs(index + 1, length, temp + str.charAt(index), str);
+    dfs(index + 1, length, temp + " " + str.charAt(index), str);
+  }
+}
---
+++ test/AllPossibleStringsTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import geeks4geeks.AllPossibleStrings;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AllPossibleStringsTest {
+
+  static AllPossibleStrings object;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    object = new AllPossibleStrings();
+  }
+
+  @Test
+  public void test() {
+    String str = "abc";
+    object.printSpace(str);
+  }
+
+}
